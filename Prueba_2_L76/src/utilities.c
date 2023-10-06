89391f2ff47067e7cf6f5181bf457cb7993eea14
lddiazcharris <lddiazcharris@gmail.com>
2023-10-06 10:45:47 -0500

    Actualizacion prueba 1

---
+++ Prueba_2_L76/src/utilities.c
@@ -0,0 +1,50 @@
+#include "utilities.h"
+
+void uart_init(  uart_port_t     uart_num, 
+                        int             baud_rate, 
+                        int             rx_buffer_size, 
+                        int             tx_buffer_size, 
+                        int             queue_size, 
+                        QueueHandle_t   *uart_queue, 
+                        int             intr_alloc_flags
+                     ){
+    
+    uart_config_t uart_config = {
+        .baud_rate = baud_rate,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+
+    ESP_ERROR_CHECK(uart_driver_install(    uart_num, 
+                                            rx_buffer_size*2, 
+                                            tx_buffer_size,
+                                            queue_size, 
+                                            uart_queue, 
+                                            intr_alloc_flags
+                                         )
+                     );
+
+    ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
+}
+
+
+
+void uart_transmit(uart_port_t uart_num, const void *src)
+{
+    uart_write_bytes(uart_num, src, strlen((const char *)src));
+}
+
+
+void uart_receive(uart_port_t uart_num, void *buf)
+{
+    uart_read_bytes(uart_num, buf, strlen((const char *)buf), portMAX_DELAY);
+}
+
+
+void delay(const TickType_t delay_ms)
+{
+    vTaskDelay( delay_ms / portTICK_PERIOD_MS);
+}
