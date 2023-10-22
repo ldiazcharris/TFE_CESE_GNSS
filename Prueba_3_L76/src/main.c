accf92a0fc5c3c102e3a7baf300f9c1db9e63a18
lddiazcharris <lddiazcharris@gmail.com>
2023-10-22 15:43:41 -0500

    Se contruyo la base del parser. No probado. Pendiente ajuste para tarea de freeRTOS.

--- Prueba_3_L76/src/main.c
+++ Prueba_3_L76/src/main.c
@@ -9,6 +9,7 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 #include "utilities.h"
+#include "esp_log.h"
 
 /************************MACROS***********************************/
 
@@ -20,17 +21,25 @@
 
 
 
-/****************DECLARACIÓN DE FUNCIONES*************************/
-
-
-
-static void uart_interrupt_task(void *params);
-
-
 
 /**************DECLARACIÓN DE VARIABLES GLOBALES*******************/
 
 static QueueHandle_t uart1_queue;
+
+static const char *TAG = "NMEA_PARSER";
+
+typedef struct {
+    float latitude;
+    float longitude;
+    char time[10];
+} GNSSData;
+
+
+/****************DECLARACIÓN DE FUNCIONES*************************/
+
+static void uart_interrupt_task(void *params);
+static void parser_nemea_task(const char *nmeaString, GNSSData *gnssData);
+
 
 
 /**********************FUNCIÓN PRINCIPAL**************************/
@@ -46,7 +55,19 @@ void app_main()
             //  (UART_NUM, TX, RX, RTS, CTS) 
     uart_set_pin(UART1,    33, 26,  14, 12);
 
-    xTaskCreate(uart_interrupt_task, "uart_interrupt_task", BUF_SIZE*4, NULL, 12, NULL);
+    xTaskCreate(    uart_interrupt_task, 
+                    "uart_interrupt_task", 
+                    BUF_SIZE*4, 
+                    NULL, 
+                    12, 
+                    NULL);
+
+    xTaskCreate(    parser_nemea_task, 
+                    "parser_nemea_task", 
+                    BUF_SIZE*4, 
+                    NULL, 
+                    12, 
+                    NULL);
 
     uart_transmit(UART1, (const void *)RMC, strlen(RMC));
 
@@ -78,6 +99,7 @@ static void uart_interrupt_task(void *params)
             case UART_DATA:
                 uart_receive(UART1, (void *) data, (uint32_t)uart_event.size);
                 sprintf((char *)print_data, "%s", data);
+                //uart_transmit(UART0,  "---", 3);
                 uart_transmit(UART0,  print_data, uart_event.size);
                 
                 break;
@@ -93,4 +115,47 @@ static void uart_interrupt_task(void *params)
     free(data);
     free(print_data);
 
+}
+
+static void parser_nemea_task(const char *nmeaString, GNSSData *gnssData){
+    // Verificar que la cadena comience con '$'
+    if (nmeaString[0] != '$') {
+        ESP_LOGW(TAG, "Cadena NMEA no válida, no comienza con '$'");
+        return;
+    }
+
+    // Utilizamos strtok para dividir la cadena en tokens usando ","
+    char *token;
+    token = strtok((char *)nmeaString, ",");
+
+    // Comprobamos si el primer token es "$GPRMC"
+    if (strcmp(token, "$GPRMC") != 0) {
+        ESP_LOGW(TAG, "Cadena NMEA no válida, no es un mensaje GPRMC");
+        return;
+    }
+
+    // Iteramos a través de los tokens
+    for (int i = 1; i < 12; i++) {
+        token = strtok(NULL, ",");
+        if (token == NULL) {
+            ESP_LOGW(TAG, "Cadena NMEA no válida, falta un campo");
+            return;
+        }
+        if (i == 1) {
+            // Obtener la hora en formato HHMMSS
+            strncpy(gnssData->time, token, 10);
+        } else if (i == 3) {
+            // Obtener la latitud en formato DDMM.MMMM
+            float lat_degrees = atof(token) / 100;
+            int lat_minutes = (int)lat_degrees;
+            float lat_seconds = (lat_degrees - lat_minutes) * 60;
+            gnssData->latitude = lat_minutes + lat_seconds;
+        } else if (i == 5) {
+            // Obtener la longitud en formato DDDMM.MMMM
+            float lon_degrees = atof(token) / 100;
+            int lon_minutes = (int)lon_degrees;
+            float lon_seconds = (lon_degrees - lon_minutes) * 60;
+            gnssData->longitude = lon_minutes + lon_seconds;
+        }
+    }
 }
