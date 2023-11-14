e78db1ed68ce5fac7c48cf9a7d33b571be51586e
lddiazcharris <lddiazcharris@gmail.com>
2023-11-14 16:47:19 -0500

    Prueba 4 exitosa.

---
+++ Prueba_4_LCD/src/main.c
@@ -0,0 +1,106 @@
+
+/************************INCLUDES*********************************/
+
+#include <stdio.h>
+#include <string.h>
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "utilities.h"
+#include "lcd_i2c_grove.h"
+
+
+/************************MACROS***********************************/
+
+#define BUF_SIZE 1024
+#define RMC "$GNRMC"
+#define JUMP " \n"
+#define UART0 UART_NUM_0
+#define UART1 UART_NUM_1
+
+/**************DECLARACIÓN DE VARIABLES GLOBALES*******************/
+
+static QueueHandle_t uart1_queue;
+static GNSSData_t quectel_l76;
+
+/****************DECLARACIÓN DE FUNCIONES*************************/
+
+static void uart_interrupt_task(void *params);
+
+/**********************FUNCIÓN PRINCIPAL**************************/
+
+void app_main()
+{
+    uart_init(UART0, 115200, BUF_SIZE * 2, 0, 0, NULL, ESP_INTR_FLAG_LEVEL1);
+    //          (UART_NUM, TX, RX, RTS, CTS)
+    uart_set_pin(UART0,     1,  3,  23,  19);
+
+    uart_init(UART1, 9600, BUF_SIZE * 2, BUF_SIZE * 2, 50, &uart1_queue, ESP_INTR_FLAG_LEVEL1); //   ESP_INTR_FLAG_IRAM
+    //          (UART_NUM, TX, RX, RTS, CTS)
+    uart_set_pin(UART1,    33, 26,  14,  12);
+
+    lcd_init(); 
+    lcd_clear(); 
+    lcd_set_RGB(255, 255, 255);
+
+    xTaskCreate(uart_interrupt_task,
+                "uart_interrupt_task",
+                BUF_SIZE * 4,
+                NULL,
+                12,
+                NULL);
+}
+
+/****************DEFINICIÓN DE FUNCIONES**************************/
+
+static void uart_interrupt_task(void *params)
+{
+    uart_event_t uart_event;
+    uint8_t *uart_recv_data = (uint8_t *)malloc(BUF_SIZE);
+    uint8_t *nmea_string = (uint8_t *)malloc(BUF_SIZE + 100);
+    uint8_t *proof_print = (uint8_t *)malloc(BUF_SIZE + 100);
+    while (1)
+    {
+        uart_transmit(UART1, (const void *)RMC, strlen(RMC));
+        if (xQueueReceive(uart1_queue, (void *)&uart_event, (TickType_t)portMAX_DELAY))
+        {
+            bzero(uart_recv_data, BUF_SIZE);
+            bzero(nmea_string, BUF_SIZE + 100);
+            bzero(proof_print, BUF_SIZE + 100);
+
+            switch (uart_event.type)
+            {
+            case UART_DATA:
+                uart_receive(UART1, (void *)uart_recv_data, (uint32_t)uart_event.size);
+                sprintf((char *)nmea_string, "%s", uart_recv_data);
+
+                uart_transmit(UART0, nmea_string, strlen((const char*)nmea_string));
+                
+                /*
+                nmea_parser((const char *)nmea_string, &quectel_l76);
+                sprintf((char *)proof_print, "Lat: %.6f, Long: %.6f \n", 
+                        quectel_l76.latitude, quectel_l76.longitude);
+                uart_transmit(UART0, proof_print, strlen((const char*)proof_print));
+                */
+                nmea_rmc_parser_r((const char *)nmea_string, &quectel_l76);
+                sprintf((char *)proof_print, "Lat: %.6f, Long: %.6f \n", 
+                        quectel_l76.latitude, quectel_l76.longitude);
+                uart_transmit(UART0, proof_print, strlen((const char*)proof_print));
+                
+                lcd_cursor(1,0);
+                lcd_write_string((char *)proof_print);
+
+                break; 
+
+            default:
+                break;
+            }
+        }
+    }
+
+    free(uart_recv_data);
+    free(nmea_string);
+}
+
